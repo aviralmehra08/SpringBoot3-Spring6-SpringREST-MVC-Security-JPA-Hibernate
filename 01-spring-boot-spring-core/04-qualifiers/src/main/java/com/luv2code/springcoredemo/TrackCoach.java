ba3349cb2d472a26d0e53c96f7db412c111c5ca5
Aviral Mehra <aviral.mehra008@gmail.com>
2023-06-27 16:13:33 +0530

    Qualifier Injection

---
+++ 01-spring-boot-spring-core/04-qualifiers/src/main/java/com/luv2code/springcoredemo/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+}

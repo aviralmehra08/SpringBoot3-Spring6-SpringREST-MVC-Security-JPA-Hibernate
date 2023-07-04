bc14f7f9f4476baf9630a1bab22bc218ccb57ec4
Aviral Mehra <aviral.mehra008@gmail.com>
2023-07-04 15:51:56 +0530

    Spring Data JPA- CRUD

---
+++ 03-spring-boot-rest-crud/03-spring-data-jpa-crud-employee/src/main/java/com/luv2code/springboot/crudDemo/dao/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.luv2code.springboot.crudDemo.dao;
+
+import com.luv2code.springboot.crudDemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}

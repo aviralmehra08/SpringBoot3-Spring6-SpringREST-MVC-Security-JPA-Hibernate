cdfe26713e54b0d2141bc9fa3a234141e521dafb
Aviral Mehra <aviral.mehra008@gmail.com>
2023-07-13 14:59:39 +0530

    Spring MVC Security - Custom Login Form with Error using Thymeleaf

--- 07-spring-boot-spring-mvc-security/01-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demoSecurity/security/DemoSecurityConfig.java
+++ 07-spring-boot-spring-mvc-security/01-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demoSecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.luv2code.springboot.demoSecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -26,5 +28,19 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE","MANAGER","ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer->
+                        configurer
+                                .anyRequest().authenticated()
+                        )
+                .formLogin(form->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                );
+        return http.build();
     }
 }
